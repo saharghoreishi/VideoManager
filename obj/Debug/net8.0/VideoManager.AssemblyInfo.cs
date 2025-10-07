25795d69d69d1aa4e2c44577596f3dbc9261dc83
Sahar Ghoreishi <saharghoreishi@gmail.com>
2025-10-07 18:37:22 +0200

    resolve conflict

--- obj/Debug/net8.0/VideoManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/VideoManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VideoManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b179f74a0e8abfba8175b235c3b3656b5b346c40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c1e77aaa68e1b8118d6380fac7e393834c42c3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("VideoManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VideoManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
