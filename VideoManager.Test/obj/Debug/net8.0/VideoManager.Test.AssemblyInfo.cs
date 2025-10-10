d21d99d7918dfff3d76f0f8c7fabd69fe565f0aa
Sahar Ghoreishi <saharghoreishi@gmail.com>
2025-10-10 14:48:00 +0200

    Add AssemblyInfo

--- VideoManager.Test/obj/Debug/net8.0/VideoManager.Test.AssemblyInfo.cs
+++ VideoManager.Test/obj/Debug/net8.0/VideoManager.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VideoManager.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae8cb8c2e19239ef33819e470fc10d2177c0f85b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+660ddfbd0c06daaac976b3b8f6c60f9ff5b172a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("VideoManager.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VideoManager.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
