1d36a020a6b18a869ff5f23446fdc227f61c0c42
Sahar Ghoreishi <saharghoreishi@gmail.com>
2025-10-10 14:51:55 +0200

    add Packages

--- VideoManager.Test/obj/Debug/net8.0/VideoManager.Test.AssemblyInfo.cs
+++ VideoManager.Test/obj/Debug/net8.0/VideoManager.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VideoManager.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae8cb8c2e19239ef33819e470fc10d2177c0f85b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0890867ab5b1a723f85993dc0d7a23cf7e3ae5c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("VideoManager.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VideoManager.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
